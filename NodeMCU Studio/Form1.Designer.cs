494356b0d416d357a1f800b3b00c6e8d0116e63a
xinshaochen <942266575@qq.com>
2016-06-07 17:54:35 +0800

    nodemcu studio1.0

---
+++ NodeMCU Studio/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace NodeMCU_Studio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textbox1 = new System.Windows.Forms.TextBox();
+            this.downloadbut = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.端口cbox = new System.Windows.Forms.ComboBox();
+            this.波特率cbox = new System.Windows.Forms.ComboBox();
+            this.连接but = new System.Windows.Forms.Button();
+            this.filelist = new System.Windows.Forms.ListBox();
+            this.openfeil = new System.Windows.Forms.Button();
+            this.savefile = new System.Windows.Forms.Button();
+            this.testtext = new System.Windows.Forms.TextBox();
+            this.readlua = new System.Windows.Forms.Button();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textbox1
+            // 
+            this.textbox1.Location = new System.Drawing.Point(250, 14);
+            this.textbox1.Multiline = true;
+            this.textbox1.Name = "textbox1";
+            this.textbox1.Size = new System.Drawing.Size(372, 282);
+            this.textbox1.TabIndex = 0;
+            // 
+            // downloadbut
+            // 
+            this.downloadbut.Location = new System.Drawing.Point(639, 293);
+            this.downloadbut.Name = "downloadbut";
+            this.downloadbut.Size = new System.Drawing.Size(54, 23);
+            this.downloadbut.TabIndex = 1;
+            this.downloadbut.Text = "下载";
+            this.downloadbut.UseVisualStyleBackColor = true;
+            this.downloadbut.Click += new System.EventHandler(this.downloadbut_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(250, 302);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(372, 14);
+            this.progressBar1.TabIndex = 2;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.端口cbox);
+            this.groupBox3.Controls.Add(this.波特率cbox);
+            this.groupBox3.Controls.Add(this.连接but);
+            this.groupBox3.Location = new System.Drawing.Point(12, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(154, 117);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "串口设置";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "端口：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 55);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 12);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "波特率：";
+            // 
+            // 端口cbox
+            // 
+            this.端口cbox.FormattingEnabled = true;
+            this.端口cbox.Location = new System.Drawing.Point(65, 18);
+            this.端口cbox.Name = "端口cbox";
+            this.端口cbox.Size = new System.Drawing.Size(60, 20);
+            this.端口cbox.TabIndex = 4;
+            this.端口cbox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.端口cbox_MouseClick);
+            // 
+            // 波特率cbox
+            // 
+            this.波特率cbox.FormattingEnabled = true;
+            this.波特率cbox.Location = new System.Drawing.Point(65, 52);
+            this.波特率cbox.Name = "波特率cbox";
+            this.波特率cbox.Size = new System.Drawing.Size(60, 20);
+            this.波特率cbox.TabIndex = 5;
+            // 
+            // 连接but
+            // 
+            this.连接but.Location = new System.Drawing.Point(6, 88);
+            this.连接but.Name = "连接but";
+            this.连接but.Size = new System.Drawing.Size(39, 23);
+            this.连接but.TabIndex = 3;
+            this.连接but.Text = "连接";
+            this.连接but.UseVisualStyleBackColor = true;
+            this.连接but.Click += new System.EventHandler(this.连接but_Click);
+            // 
+            // filelist
+            // 
+            this.filelist.FormattingEnabled = true;
+            this.filelist.ItemHeight = 12;
+            this.filelist.Location = new System.Drawing.Point(172, 14);
+            this.filelist.Name = "filelist";
+            this.filelist.Size = new System.Drawing.Size(72, 280);
+            this.filelist.TabIndex = 13;
+            this.filelist.MouseClick += new System.Windows.Forms.MouseEventHandler(this.filelist_MouseClick);
+            this.filelist.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.filelist_MouseDoubleClick);
+            // 
+            // openfeil
+            // 
+            this.openfeil.Location = new System.Drawing.Point(628, 14);
+            this.openfeil.Name = "openfeil";
+            this.openfeil.Size = new System.Drawing.Size(65, 23);
+            this.openfeil.TabIndex = 14;
+            this.openfeil.Text = "打开文件";
+            this.openfeil.UseVisualStyleBackColor = true;
+            this.openfeil.Click += new System.EventHandler(this.openfeil_Click);
+            // 
+            // savefile
+            // 
+            this.savefile.Location = new System.Drawing.Point(629, 43);
+            this.savefile.Name = "savefile";
+            this.savefile.Size = new System.Drawing.Size(64, 23);
+            this.savefile.TabIndex = 15;
+            this.savefile.Text = "保存文件";
+            this.savefile.UseVisualStyleBackColor = true;
+            this.savefile.Click += new System.EventHandler(this.savefile_Click);
+            // 
+            // testtext
+            // 
+            this.testtext.Location = new System.Drawing.Point(699, 16);
+            this.testtext.Multiline = true;
+            this.testtext.Name = "testtext";
+            this.testtext.Size = new System.Drawing.Size(372, 282);
+            this.testtext.TabIndex = 16;
+            // 
+            // readlua
+            // 
+            this.readlua.Location = new System.Drawing.Point(639, 264);
+            this.readlua.Name = "readlua";
+            this.readlua.Size = new System.Drawing.Size(54, 23);
+            this.readlua.TabIndex = 17;
+            this.readlua.Text = "读取";
+            this.readlua.UseVisualStyleBackColor = true;
+            this.readlua.Click += new System.EventHandler(this.readlua_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1082, 320);
+            this.Controls.Add(this.readlua);
+            this.Controls.Add(this.testtext);
+            this.Controls.Add(this.savefile);
+            this.Controls.Add(this.openfeil);
+            this.Controls.Add(this.filelist);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.downloadbut);
+            this.Controls.Add(this.textbox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textbox1;
+        private System.Windows.Forms.Button downloadbut;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox 端口cbox;
+        private System.Windows.Forms.ComboBox 波特率cbox;
+        private System.Windows.Forms.Button 连接but;
+        private System.Windows.Forms.ListBox filelist;
+        private System.Windows.Forms.Button openfeil;
+        private System.Windows.Forms.Button savefile;
+        private System.Windows.Forms.TextBox testtext;
+        private System.Windows.Forms.Button readlua;
+    }
+}
+
---
+++ NodeMCU Studio/Form1.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NodeMCU_Studio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false;
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                端口cbox.Items.Add(s);
+            }
+            //端口cbox.SelectedIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = "NodeMCU Studio";
+            this.Text = "超声波";
+
+            波特率cbox.Items.Add("9600");
+            波特率cbox.Items.Add("115200");
+            波特率cbox.SelectedIndex = 0;
+            //downloadbut.Enabled = false;
+            textbox1.ScrollBars = ScrollBars.Both;
+            serialPort1.DataReceived += new SerialDataReceivedEventHandler(portDataReceived);
+            
+        }
+
+        string readbuf;
+        private void portDataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            byte[] buf = new byte[1024];
+            int len = serialPort1.Read(buf, 0, 1024);
+            readbuf += Encoding.Default.GetString(buf);
+            readlua.Text = readbuf;
+
+        }
+            bool buts = false;
+        private void 连接but_Click(object sender, EventArgs e)
+        {
+
+            buts = !buts;
+            if (buts == true)
+            {
+
+                try
+                {
+                    
+                    serialPort1.PortName = 端口cbox.Text;
+                    serialPort1.BaudRate = Convert.ToInt32(波特率cbox.Text, 10);
+                    serialPort1.Parity = Parity.None;
+                    serialPort1.DataBits = 8;
+                    serialPort1.StopBits = StopBits.One;
+                    serialPort1.Open();
+                    连接but.Text = "断开";
+                   downloadbut.Enabled = true;
+
+
+                }
+                catch
+                {
+                    MessageBox.Show("端口错误", "错误");
+                }
+
+
+
+            }
+            else
+            {
+                try
+                {
+                    serialPort1.Close();
+                    连接but.Text = "连接";
+                }
+                catch
+                {
+
+                    throw;
+                }
+
+            }
+        }
+        private void 端口cbox_MouseClick(object sender, MouseEventArgs e)
+        {
+            端口cbox.Items.Clear();
+            foreach (string s in SerialPort.GetPortNames())
+            {
+                端口cbox.Items.Add(s);
+            }
+            //端口cbox.SelectedIndex = 0;
+        }
+
+        uint l = 0;
+        List<string> filestr = new List<string>();
+        List<string> text = new List<string>(); 
+        private void openfeil_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "Lua files (*.lua)|*.lua|All files (*.*)|*.*"; //过滤文件类型
+            fd.Multiselect = true;//多选文件
+            fd.InitialDirectory = Application.StartupPath+"lua";//设定初始目录
+            //fd.ShowReadOnly = true; //设定文件是否只读
+            DialogResult r = fd.ShowDialog();
+            if(r==DialogResult.OK)
+            {
+                
+                foreach (string filename in fd.FileNames)
+                {
+                    FileStream fs = new FileStream(filename,FileMode.Open);
+                    StreamReader Reader = new StreamReader(fs);
+                    //while (!Reader.EndOfStream)
+                    //{
+                        text.Add(Reader.ReadToEnd());
+                   // }
+                   
+                    Reader.Close();
+                }
+                foreach (string str in fd.SafeFileNames)
+                {
+                    filelist.Items.Add(str);
+                    
+                }
+                foreach(string str in fd.FileNames)
+                {
+                    filestr.Add(str);
+                }
+
+                //string[] strs = fd.SafeFileNames;
+                //filestr = fd.FileNames;
+                //foreach (string str in strs)
+                //{
+                //    filelist.Items.Add(str);
+                //}
+                filelist.SelectedIndex = 0;
+                lastSelectedIndex = 0;
+                textbox1.Text = text[0];
+
+            }
+        }
+        int lastSelectedIndex;
+        private void filelist_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (filelist.SelectedItem != null &&  filelist.Items.Count> 0 && lastSelectedIndex!=filelist.SelectedIndex)
+            {
+                text[lastSelectedIndex] = textbox1.Text;
+                lastSelectedIndex = filelist.SelectedIndex;
+                textbox1.Text = text[filelist.SelectedIndex];
+               // FileStream fs = new FileStream(filestr[filelist.SelectedIndex],FileMode.Open);
+               // StreamReader Reader = new StreamReader(fs);
+               // textbox1.Clear();
+               //while(!Reader.EndOfStream)
+               // {
+               //     textbox1.Text += Reader.ReadToEnd(); 
+               // }
+               // Reader.Close();
+
+            }
+        }
+
+        uint readmode = 0;
+        private void downloadbut_Click(object sender, EventArgs e)
+        {
+            text[filelist.SelectedIndex] = textbox1.Text;
+            string strtext=textbox1.Text;
+            strtext = strtext.Replace("\"","\\\"");
+            string[] ContenLines = strtext.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);//忽略空行
+
+            
+           testtext.Text += "file.remove(\"" + filelist.SelectedItem + "\")"+"\r\n";
+            testtext.Text += "file.open(\"" + filelist.SelectedItem + "\",\"w+\")" + "\r\n";
+
+           // serialPort1.WriteLine("file.remove(\"" + filelist.SelectedItem+"\")");
+           // serialPort1.WriteLine("file.open(\"" + filelist.SelectedItem + "\",\"w+\")");
+
+
+            foreach (string str in ContenLines)
+            {
+                testtext.Text += "file.writeline(\"" + str + "\")" + "\r\n";
+                //serialPort1.WriteLine("file.writeline(\"" + str + "\")" + "\r\n");
+            }
+
+            testtext.Text += "file.close()";
+            
+           // serialPort1.WriteLine("file.close()");
+
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+        private void savefile_Click(object sender, EventArgs e)
+        {
+            int l = 0;
+            text[filelist.SelectedIndex] = textbox1.Text;
+            foreach(string str in filestr)
+            {
+                File.WriteAllText(str,text[l]);
+                l++;
+            }
+        }
+
+        private void filelist_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (filelist.SelectedItem != null && filelist.Items.Count > 0 && lastSelectedIndex != filelist.SelectedIndex)
+            {
+                text[lastSelectedIndex] = textbox1.Text;
+                lastSelectedIndex = filelist.SelectedIndex;
+                textbox1.Text = text[filelist.SelectedIndex];
+                // FileStream fs = new FileStream(filestr[filelist.SelectedIndex],FileMode.Open);
+                // StreamReader Reader = new StreamReader(fs);
+                // textbox1.Clear();
+                //while(!Reader.EndOfStream)
+                // {
+                //     textbox1.Text += Reader.ReadToEnd(); 
+                // }
+                // Reader.Close();
+
+            }
+        }
+
+        private void readlua_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
